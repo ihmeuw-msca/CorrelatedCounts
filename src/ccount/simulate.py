b8283dcdc0745ee0efa8b323938fa1b25a17a1f0
Marlena Bannick <mnorwood@uw.edu>
2019-11-06 10:11:55 -0800

    Finished simulation class.

--- src/ccount/simulate.py
+++ src/ccount/simulate.py
@@ -1,23 +1,25 @@
-import numpy as np
-from numpy.random import multivariate_normal, binomial
-from numpy import identity
+from numpy.random import multivariate_normal, binomial, poisson
+from numpy import identity, exp, matmul, zeros, transpose as t
 
 
 class Simulation:
     def __init__(self, n, J, d):
         """
-        Simulation setup for creating correlated count data for mortality and incidence.
+        Simulation setup for creating correlated count data.
+        There are n individuals, J correlated count bins, and d[j] fixed effects for each J.
 
-        There are n individuals, k correlated count bins, and d_k fixed effects.
-
-        :param n: (int) number of individuals in the simulation
-        :param J: (int) number of categories for the correlated counts
-        :param d: (List[int]) list of integers (of length J) representing the length of the
-            fixed effects vector for each of the J categories
+        Parameters:
+            n: (int) number of individuals in the simulation
+            J: (int) number of categories for the correlated counts
+            d: (List[int]) list of integers (of length J) representing the length of the
+                fixed effects vector for each of the J categories
 
         Usage:
-        >>> s = Simulation(n=100, k=2, d_k=[2, 2])
-        >>> s.update_params()
+        >>> s = Simulation(n=100, k=2, d=[2, 2])
+        >>> s.simulate()
+
+        >>> s.update_params(p=0.2)
+        >>> s.simulate()
         """
         # Dimension parameters
         assert type(n) is int
@@ -32,22 +34,27 @@ class Simulation:
         # Baseline simulation parameters
         self.p = 0.5
         self.D = identity(n=self.J)
-        self.x = [multivariate_normal(mean=np.zeros(d_j), cov=identity(n=d_j), size=self.n) for d_j in self.d]
-        self.beta = [multivariate_normal(mean=np.zeros(d_j), cov=identity(n=d_j), size=1) for d_j in self.d]
+        self.x = [multivariate_normal(mean=zeros(d_j), cov=identity(n=d_j), size=self.n) for d_j in self.d]
+        self.beta = [multivariate_normal(mean=zeros(d_j), cov=identity(n=d_j), size=1) for d_j in self.d]
 
         # Simulation results
-        self.p_sim = None
-        self.y_sim = None
         self.u_sim = None
+        self.u = None
+        self.theta_sim = None
+
+        self.y_zeros = None
+        self.y_poisson = None
+        self.y_zip = None
 
     def update_params(self, x=None, beta=None, p=None, D=None):
         """
         Update the parameters from baseline for the simulation
 
-        :param p: (int) probability of observing a 0
-        :param x: (List[np.ndarray]) list of length J of np.ndarray with dimension (n, d[j])
-        :param beta: (List[np.array]) list of length J with np.arrays of dimension d[j]
-        :param D: (np.ndarray) 2D variance-covariance matrix of square dimension J for the random effects
+        Parameters:
+            p: (int) probability of observing a 0
+            x: (List[np.ndarray]) list of length J of np.ndarray with dimension (n, d[j])
+            beta: (List[np.array]) list of length J with np.arrays of dimension d[j]
+            D: (np.ndarray) 2D variance-covariance matrix of square dimension J for the random effects
         """
         self.x = x if x else self.x
         self.beta = beta if beta else self.beta
@@ -57,8 +64,28 @@ class Simulation:
     def simulate(self):
         """
         Make one simulation of the outcome y based on current parameters.
-        :return:
+
+        Attributes:
+            self.u_sim: (np.ndarray) 2D array of shape (n, J), n random realizations of the random effect U where
+                .. math::
+                    U \sim N(0, D)
+            self.theta_sim: (List[np.ndarray]) list of length J where
+                .. math::
+                    theta_sim_{j} = e^{x_{j} beta_{j}^T + u_sim_{j}^T}
+            self.y_zeros: (np.ndarray) 2D array of shape (J, n) that induces structural zeroes
+            self.y_poisson: (np.ndarray) 2D array of shape (J, n) with poisson realizations with mean theta_sim
+                .. math::
+                    y_poisson_{i, j} \sim Poisson(lambda=theta_sim_{i, j})
+            self.z_zip: (np.ndarray) 2D array of shape (J, n) with poisson realizations masked
+                by the structural zeros from p_sim
         """
-        self.p_sim = binomial(size=self.n, p=self.p, n=1)
-        self.u_sim = multivariate_normal(size=self.n, mean=np.zeros(self.J), cov=self.D)
-        
+        self.u_sim = multivariate_normal(size=self.n, mean=zeros(self.J), cov=self.D)
+        self.theta_sim = [exp(
+            matmul(self.x[j], t(self.beta[j])).flatten() + t(self.u_sim)[j]
+        ) for j in range(self.J)]
+
+        self.y_zeros = 1 - binomial(size=(self.J, self.n), p=self.p, n=1)
+        self.y_poisson = poisson(lam=self.theta_sim)
+        self.y_zip = self.y_zeros * self.y_poisson
+
+        return self.y_zip
