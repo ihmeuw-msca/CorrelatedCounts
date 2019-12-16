15c586596c1bea60315f86e53bb98059d5ecc9dd
Marlena Bannick <mnorwood@uw.edu>
2019-12-16 15:35:41 -0800

    likelihood class to keep things clean

---
+++ src/ccount/likelihoods.py
@@ -0,0 +1,103 @@
+import numpy as np
+from scipy.special import gammaln
+
+
+class NegLogLikelihoods:
+
+    @staticmethod
+    def hurdle_poisson(Y, P):
+        """
+        Hurdle Poisson likelihood.
+        Structural Zeroes induced by binomial distribution, then Non-Zeroes induced
+        by truncated Poisson model.
+
+        Args:
+            Y: observed data
+            P: list with the following elements:
+                0: the probability of a zero
+                1: mean of the Poisson distribution
+        """
+        assert P.shape[0] == 2
+        p = P[0]
+        theta = P[1]
+        ll = (
+            (np.log(p)) * (Y == 0) +
+            (np.log(1 - p) - theta + Y * np.log(theta) - np.log(1 - np.exp(-theta))) * (Y > 0)
+        )
+        return -ll
+
+    @staticmethod
+    def zi_poisson(Y, P):
+        """
+        Zero-Inflated Poisson likelihood.
+        Structural Zeroes induced by either binomial distribution, additional zeroes
+        from the Poisson distribution.
+
+        Args:
+            Y: observed data
+            P: list with the following elements:
+                0: the probability of a structural zero
+                1: mean of the Poisson distribution
+
+        Returns:
+            negative log likelihood
+        """
+        assert P.shape[0] == 2
+        p = P[0]
+        theta = P[1]
+        ll = (
+            (np.log((p + (1 - p) * np.exp(-theta)))) * (Y == 0) +
+            (np.log(1 - p) - theta + Y * np.log(theta)) * (Y > 0)
+        )
+        return -ll
+
+    @staticmethod
+    def zi_nbinom(Y, P):
+        """
+        Zero-Inflated Negative Binomial likelihood.
+        Structural Zeroes induced by either binomial distribution, additional zeroes
+        from the Negative Binomial distribution.
+
+        Args:
+            Y: observed data
+            P: list with the following elements:
+                0: the probability of a structural zero
+                1: mean of the Poisson distribution
+                2: over-dispersion parameter for negative binomial
+        """
+        assert P.shape[1] == 3
+        p = P[0]
+        theta = P[1]
+        k = P[2]
+        ll = (
+            (np.log(p + (1 - p) * (1 + k * theta) ** (-1 / k))) * (Y == 0) +
+            (np.log(1 - p) +
+             gammaln(Y + k ** (-1)) -
+             gammaln(k ** (-1)) -
+             k ** (-1) * np.log(1 + k * theta) -
+             Y * np.log(1 + (theta * k) ** (-1))) * (Y > 0)
+        )
+        return -ll
+
+    @staticmethod
+    def nbinom(Y, P):
+        """
+        Negative Binomial likelihood.
+
+        Args:
+            Y: observed data
+            P: list with the following elements:
+                0: the probability of a structural zero
+                1: mean of the Poisson distribution
+                2: over-dispersion parameter for negative binomial
+        """
+        assert P.shape[1] == 2
+        theta = P[1]
+        k = P[2]
+        ll = (
+            gammaln(Y + k ** (-1)) -
+            gammaln(k ** (-1)) -
+            k ** (-1) * np.log(1 + k * theta) -
+            Y * np.log(1 + (theta * k) ** (-1))
+        )
+        return -ll
