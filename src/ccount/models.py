abf6463a79dab0ece27475bfe6f6a8cbfb5f051d
Marlena Bannick <mnorwood@uw.edu>
2019-12-16 15:35:55 -0800

    Added negative binomial model

--- src/ccount/models.py
+++ src/ccount/models.py
@@ -1,25 +1,31 @@
 import numpy as np
 from ccount.core import CorrelatedModel
+from ccount.likelihoods import NegLogLikelihoods
 import logging
 
 LOG = logging.getLogger(__name__)
 
 
-class HurdlePoissonModel(CorrelatedModel):
+class HurdlePoisson(CorrelatedModel):
     """
     A Hurdle Model. Has a binomial model
     for the proportion of zeros, and a zero-truncated
     Poisson for the
     """
-    def __init__(self, m, d, Y, X):
+    def __init__(self, m, d, Y, X, group_id=None):
         LOG.info("Initializing a Hurdle Poisson Model.")
         assert len(d) == 2
         assert len(X) == 2
         super().__init__(
-            m=m, n=2, d=d, Y=Y.astype(np.number), X=X,
+            m=m, n=2, d=d, Y=Y.astype(np.number), X=X, group_id=group_id,
             l=2, g=[lambda x: np.exp(x) / (1 + np.exp(x)), np.exp],
-            f=hurdle_neg_log_likelihood
+            f=NegLogLikelihoods.hurdle_poisson
         )
+
+    def fitted_values(self):
+        p = self.P[0]
+        theta = self.P[1]
+        return (1 - p) * theta / (1 - np.exp(-theta))
 
 
 class ZeroInflatedPoisson(CorrelatedModel):
@@ -35,49 +41,38 @@ class ZeroInflatedPoisson(CorrelatedModel):
     >>> zp = ZeroInflatedPoisson(m=ps.m, d=D, Y=Y, X=X)
     >>> zp.optimize_params()
     """
-    def __init__(self, m, d, Y, X):
+    def __init__(self, m, d, Y, X, group_id=None):
         LOG.info("Initializing a Zero-Inflated Poisson Model")
         assert len(d) == 2
         assert len(X) == 2
         super().__init__(
-            m=m, n=2, d=d, Y=Y.astype(np.number), X=X,
+            m=m, n=2, d=d, Y=Y.astype(np.number), X=X, group_id=group_id,
             l=2, g=[lambda x: np.exp(x) / (1 + np.exp(x)), np.exp],
-            f=zip_neg_log_likelihood
+            f=NegLogLikelihoods.zi_poisson
         )
 
     def fitted_values(self):
-        return (1 - self.P[0]) * self.P[1]
+        p = self.P[0]
+        theta = self.P[1]
+        return (1 - p) * theta
 
 
-def hurdle_neg_log_likelihood(Y, P):
+class NegativeBinomial(CorrelatedModel):
     """
-    The negative log likelihood for
-    hurdle-Poisson.
-
-    Parameters
-    ----------
-    Y : array_like
-    P : list
+    A Negative Binomial Model.
     """
-    p = P[0]
-    theta = P[1]
-    likelihood = (p * (Y == 0) +
-                  ((1 - p) * np.exp(-theta) * theta ** Y / (1 - np.exp(-theta))) * (Y > 0))
-    return -np.log(likelihood)
+    def __init__(self, m, d, Y, X, group_id=None):
+        LOG.info("Initializing a negative binomial model.")
+        assert len(d) == 2
+        assert len(X) == 2
+        super().__init__(
+            m=m, n=2, d=d, Y=Y.astype(np.number), X=X, group_id=group_id,
+            l=2, g=[lambda x: np.exp(x) / (1 + np.exp(x)), np.exp],
+            f=NegLogLikelihoods.nbinom
+        )
 
+    def fitted_values(self):
+        theta = self.P[0]
+        k = self.P[1]
+        return theta
 
-def zip_neg_log_likelihood(Y, P):
-    """
-    The negative log likelihood for
-    zero-inflated Poisson.
-
-    Parameters
-    ----------
-    Y : array_like
-    P : list
-    """
-    p = P[0]
-    theta = P[1]
-    likelihood = ((p + (1 - p) * np.exp(-theta)) * (Y == 0) +
-                  ((1 - p) * np.exp(-theta) * theta ** Y) * (Y > 0))
-    return -np.log(likelihood)
